a2ede12473ff02a6c9703463eb22f3fc9bad0b67
Sergei Popov <Serji.popov@gmail.com>
2013-07-13 23:42:43 +0400

    Finally it's works!

--- js/popup.js
+++ js/popup.js
@@ -1,28 +1,53 @@
 $(document).ready(function(){
 
-    getLangs("ru")
+    getLangs("ru");
 
+    
 
     $(document).on("click", "ul:first a", function(){
         var lang=$(this).attr("href");
         $(this).parent().parent().parent().find('b').text(lang[1]+lang[2]);
 
-        getLangs(lang[1]+lang[2]);
+        getLangs(lang[1]+lang[2], null);
+    });
+
+
+    $(document).on("click", "ul:last a", function(){
+        var lang=$(this).attr("href");
+        $(this).parent().parent().parent().find('b').text(lang[1]+lang[2]);
     });
 
     //Switch
     $("form #switch").click(function(eO){
-        var f=$("b:first").text();
-        var l=$("b:last").text();
+        
+        $('input:first').focus();
 
-        $("b:first").text(l);
-        $("b:last").text(f);
+        var first=$("b:first").text();
+        var last= $("b:last").text();
+        
+        $("b:first").text(last);
+        
+        getLangs( last , first);
+
     });
 
+    //Translate
+    $('input').keydown(function(e){ 
+        
+        var code = e.which; // recommended to use e.which, it's normalized across browsers
+        
+        if(code==13)e.preventDefault();
+        
+        if(code==32||code==13){
+        
+            translate();
+        
+        }
+    });
 
 });
 
-function getLangs(lang){
+function getLangs( lang , checkChoise ){
 
     $.getJSON('https://translate.yandex.net/api/v1.5/tr.json/getLangs?key=trnsl.1.1.20130712T190923Z.a2e7430f401fe564.0c45398bb1c49532aee8be7b8bba4043d97410d2&ui='+lang, function(data) {
         
@@ -45,23 +70,32 @@ function getLangs(lang){
         });
 
         var languagesTo='';
-
+        
         $.each(from, function(i,val) {
             
             if (val == $("b:first").text().toLowerCase() ){
+                if (checkChoise==null) {checkChoise=to[i];}
                 languagesTo=languagesTo+'<li><a href="#'+ to[i] +'">' + data.langs[to[i]] + '</a></li>';
             } 
 
         });
 
         $("ul:last").html(languagesTo);
-
-        //$('textarea').val();
-        //$('textarea').text(to.join('\n'))
+        $("b:last").text(checkChoise);
 
     });
 }
 
-function detectLang(){
+function translate(){
+
+    var from=$('b:first').text();
+    var to=$('b:last').text();
+
+    var text=$('input').first().val();
+
+    $.getJSON('https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20130712T190923Z.a2e7430f401fe564.0c45398bb1c49532aee8be7b8bba4043d97410d2&lang='+from+'-'+to+'&text='+text, function(data) {
         
+        $('input').last().val(data.text);
+
+    });
 }
