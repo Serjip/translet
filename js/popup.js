370abe725ca9a4b1fe5c8daa9c26d39244f42d78
Sergei Popov <Serji.popov@gmail.com>
2013-07-13 22:04:51 +0400

    Dynamic load languages "to"

--- js/popup.js
+++ js/popup.js
@@ -7,16 +7,16 @@ $(document).ready(function(){
         var lang=$(this).attr("href");
         $(this).parent().parent().parent().find('b').text(lang[1]+lang[2]);
 
-        getLangs($("ul:last"),lang[1]+lang[2]);
+        getLangs(lang[1]+lang[2]);
     });
 
     //Switch
     $("form #switch").click(function(eO){
-        var f=$("b").first().text();
-        var l=$("b").last().text();
+        var f=$("b:first").text();
+        var l=$("b:last").text();
 
-        $("b").first().text(l);
-        $("b").last().text(f);
+        $("b:first").text(l);
+        $("b:last").text(f);
     });
 
 
@@ -26,28 +26,37 @@ function getLangs(lang){
 
     $.getJSON('https://translate.yandex.net/api/v1.5/tr.json/getLangs?key=trnsl.1.1.20130712T190923Z.a2e7430f401fe564.0c45398bb1c49532aee8be7b8bba4043d97410d2&ui='+lang, function(data) {
         
-        var languages='';
+        var languagesFrom='';
 
         $.each(data.langs, function(key, val) {
-            languages=languages+'<li><a href="#'+ key +'">' + val + '</a></li>';        
+            languagesFrom=languagesFrom+'<li><a href="#'+ key +'">' + val + '</a></li>';
         });
 
-        $("ul:first").html(languages);
-
-
+        $("ul:first").html(languagesFrom);
 
         var from = new Array(); 
-
         var to= new Array();
         
         $.each(data.dirs, function(i,val) {
             
-            from[i] = val[0]+val[1];
-            
+            from[i] = val[0]+val[1];           
             to[i] = val[3]+val[4];
         
         });
 
+        var languagesTo='';
+
+        $.each(from, function(i,val) {
+            
+            if (val == $("b:first").text().toLowerCase() ){
+                languagesTo=languagesTo+'<li><a href="#'+ to[i] +'">' + data.langs[to[i]] + '</a></li>';
+            } 
+
+        });
+
+        $("ul:last").html(languagesTo);
+
+        //$('textarea').val();
         //$('textarea').text(to.join('\n'))
 
     });
